a8fc2d12c904f04289b9c40cee98b3af15ca24c3
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-06-25 19:33:21 +0800

    Update StartGame.php

--- SkyWars/src/SkyWars/Tasks/StartGame.php
+++ SkyWars/src/SkyWars/Tasks/StartGame.php
@@ -38,11 +38,11 @@ class StartGame extends PluginTask{
             $this->plugin->game[$this->gameNumber."-open"] = false;
             foreach($this->getServer()->getLevelByName($levelName)->getPlayers() as $p){
                 $this->getServer()->getLevelByName("game-".$this->gameNumber)->setBlock(new Vector3($p->getX(),$p->getY()-1,$p->getZ()),Block::get(0,0));
-                $p->sendMessage(TextFormat::GREEN."[SkyWars] Game " . $this->gameNumber . " has started!");
+                $p->sendPopup(TextFormat::GOLD . TextFormat::BOLD . "The game has started!");
             }
         }else{
             foreach($this->getServer()->getLevelByName($levelName)->getPlayers() as $p) {
-                $p->sendPopup(TextFormat::BLUE . "[SkyWars] Game " . $this->gameNumber . " starting in " . TextFormat::GREEN . $this->time);
+                $p->sendPopup(TextFormat::GOLD . TextFormat::BOLD . "The game starting in " . TextFormat::BOLD . TextFormat::GREEN . $this->time . TextFormat::GOLD . TextFormat::BOLD . ".");
             }
         }
 
